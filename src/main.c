e97c297df04d0b1d9ecd7ed5d50fa99abca4aabd
Alessandro-Salerno <OxyGen05@outlook.it>
2024-09-18 00:53:16 +0200

    first working version template

--- src/main.c
+++ src/main.c
@@ -20,18 +20,128 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+#include <dlfcn.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+
+void run_c_file(const char *c_file_path) {
+  const char *command     = "gcc -shared -fPIC ";
+  int         so_path_len = strlen(c_file_path) + 4;
+  char       *so_path     = calloc(so_path_len, sizeof(char));
+  sprintf(so_path, "%s%s", c_file_path, ".so");
+
+  char *full_command = calloc(
+      strlen(command) + so_path_len + strlen(c_file_path) + 1, sizeof(char));
+  sprintf(full_command, "%s %s -o %s", command, c_file_path, so_path);
+
+  (void)system(full_command);
+
+  void *handle = dlopen(so_path, RTLD_LAZY);
+  if (!handle) {
+    printf("Error 1\n");
+    goto cleanup;
+  }
+
+  int (*htmc)() = dlsym(handle, "htmc");
+  if (!htmc) {
+    printf("Error 2\n");
+    goto cleanup;
+  }
+
+  htmc();
+
+cleanup:
+  free(full_command);
+  free(so_path);
+}
+
+void parse_and_run(FILE *f, const char *file_path, int line) {
+  int   path_len      = strlen(file_path);
+  char *tmp_file_path = calloc(path_len + 16, sizeof(char));
+  sprintf(tmp_file_path, "./tmp/%s.%d.c", file_path, line);
+  for (char *cp = tmp_file_path + 7, ch; (ch = *cp); cp++) {
+    if ('/' == ch) {
+      *cp = '_';
+      break;
+    }
+  }
+
+  // FILE *c_file = fopen(tmp_file_path, "r");
+  // if (c_file) {
+  //   run_c_file(tmp_file_path);
+  //   goto cleanup;
+  // }
+
+  FILE *c_file = fopen(tmp_file_path, "w");
+  fprintf(c_file, "#include <stdio.h>\nint htmc() {\n");
+
+  char c;
+  char last = 0;
+  while (0 != fread(&c, 1, 1, f)) {
+    if ('>' == c && '?' == last) {
+      fprintf(c_file, "\nreturn 0;}");
+      fclose(c_file);
+      run_c_file(tmp_file_path);
+      goto cleanup;
+    } else if ('?' == last) {
+      fwrite(&last, 1, 1, c_file);
+    }
+    if ('?' != c) {
+      fwrite(&c, 1, 1, c_file);
+    }
+
+    last = c;
+  }
+
+  fclose(c_file);
+
+cleanup:
+  free(tmp_file_path);
+}
+
+void htmc_find_segments(FILE *f, const char *file_path) {
+  char c;
+  char last         = 0;
+  bool found_open   = false;
+  bool stop_emitter = false;
+  int  line         = 1;
+  while (0 != fread(&c, 1, 1, f)) {
+    if ('<' == c) {
+      stop_emitter = true;
+    }
+    if ('>' == c && !found_open) {
+      stop_emitter = false;
+    }
+    if ('?' == c && '<' == last) {
+      found_open = true;
+    } else if ('<' == last) {
+      stop_emitter = false;
+      putchar(last);
+    }
+    if (!stop_emitter) {
+      putchar(c);
+    }
+    if ('\n' == c) {
+      line++;
+    }
+    if (found_open && 'c' == last && '>' == c) {
+      parse_and_run(f, file_path, line);
+      found_open   = false;
+      stop_emitter = false;
+    }
+    last = c;
+  }
+}
 
 int main(int argc, char *argv[]) {
-  int fd[2];
-  pipe(fd);
-  char *s = "test";
-  int   l = strlen(s);
-  write(fd[1], s, l);
-  char out[20] = {0};
-  read(fd[0], out, l);
-  printf("%s", out);
+  const char *file_path = "examples/test.htmc";
+  if (argc > 2) {
+    file_path = argv[1];
+  }
+
+  FILE *f = fopen(file_path, "r");
+  htmc_find_segments(f, file_path);
+  fclose(f);
 }
