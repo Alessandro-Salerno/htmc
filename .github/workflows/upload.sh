a766ad9112972b5d4a35c53abbf9e5e49afe2510
Alessandro Salerno <67583725+Alessandro-Salerno@users.noreply.github.com>
2024-10-17 00:28:25 +0200

    Update upload.sh

--- .github/workflows/upload.sh
+++ .github/workflows/upload.sh
@@ -1,15 +1,15 @@
 git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
 git config --local user.name "github-actions[bot]"
 git checkout --orphan latest-linux-bundle
+git rm -rf *.md
 cp .github/workflows/latest-linux-bundle.md ./README.md
 git rm -rf .github
 git rm -rf cgi-ws
 git rm -rf src
-git rm -rf *.md
-git rm .gitattributes
-git rm  gitignore
-git rm .clang-format
-git rm Makefile
+git rm -rf .gitattributes
+git rm -rf  gitignore
+git rm -rf .clang-format
+git rm -rf Makefile
 git add bin/*
 git add include/*
 git add examples/*
