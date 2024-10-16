c99c97ceb9f55381d21bafe2d4b4a892e998f1b5
Alessandro Salerno <67583725+Alessandro-Salerno@users.noreply.github.com>
2024-10-17 00:06:33 +0200

    Create upload.sh

---
+++ .github/workflows/upload.sh
@@ -0,0 +1,14 @@
+git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
+git config --local user.name "github-actions[bot]"
+git checkout --orphan latest-linux-bundle
+git rm -rf .github
+git rm -rf *.md
+git rm -rf .gitattributes
+git rm -rf .gitignore
+git rm -rf .clang-format
+git rm -rf src
+git add bin/*
+git add include/*
+git add examples/*
+git commit -m "new bundle"
+git push
